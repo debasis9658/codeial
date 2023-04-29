8bdc7347340598cbedc29146a6b87e830bcf3ecb
Debasish Biswal <debasisjkbk@gmail.com>
2023-04-29 21:07:02 +0530

    Add user post with his/her name

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,15 @@
+const Post = require('../models/post');
 module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+    
+    Post.find({}).populate('user').then((posts)=>{
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
     });
+
+    
+    
 }
