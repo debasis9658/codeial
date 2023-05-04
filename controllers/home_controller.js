e5c649935a851d280fb82d1bc356ea60aa64e154
Debasish Biswal <debasisjkbk@gmail.com>
2023-05-04 13:56:03 +0530

    Tried debuggoing the code for rectifying the comments model

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,15 @@ module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
     
-    Post.find({}).populate('user').then((posts)=>{
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .then((posts)=>{
         return res.render('home', {
             title: "Codeial | Home",
             posts: posts
