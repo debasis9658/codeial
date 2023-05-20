aee91c600bf14dc7b26981673353b3ac2b26bd3b
Debasish <debasisjkbk@gmail.com>
2023-05-20 10:31:12 +0530

    Added APIs for posts destruction and user authentication using JWT

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+const User = require('../models/user');
+
+let opts = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey : 'codeial'
+}
+
+passport.use(new JWTStrategy(opts, function(jwtPayLoad, done){
+    User.findById(jwtPayLoad).then((user)=>{
+        if(user){
+            return done(null, user);
+        }
+        else{
+            return done(null, false);
+        }
+    })
+    .catch((err)=>{
+        console.log('Error in finding user from JWT');
+        return;
+    });
+}));
+
+module.exports = passport;
