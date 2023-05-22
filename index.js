2b19ce0b2f58a5d4563115e6973a6a36eac4c8ac
Debasish <debasisjkbk@gmail.com>
2023-05-22 14:40:04 +0530

    Added chatroom feature using web sockets

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
+// setup the chat server to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(3000);
+console.log('chat server is listening on port 3000');
+
+
+
+
 app.use(sassMiddleware({
     src: './assets/scss',
     dest: './assets/css',
---
+++ models/friendship.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const friendshipSchema = new mongoose.Schema({
+    //This user who sent this request
+    from_user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    //The user who accepted this request, the naming is just to understand, otherwise, the user won't see a difference
+    to_user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
+
+const Friendship = mongoose.model('Friendship', friendshipSchema);
+
+module.exports = Friendship;
